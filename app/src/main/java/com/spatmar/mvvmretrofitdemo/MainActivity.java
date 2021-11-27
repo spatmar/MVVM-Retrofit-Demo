6b86b29cb2ae41cece0a67b8fc453e157afc550e
sPatmar <spatmar@gmail.com>
2021-11-27 10:24:08 +0300

    получаем данные

--- app/src/main/java/com/spatmar/mvvmretrofitdemo/MainActivity.java
+++ app/src/main/java/com/spatmar/mvvmretrofitdemo/MainActivity.java
@@ -4,11 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.spatmar.mvvmretrofitdemo.model.MovieApiResponse;
+import com.spatmar.mvvmretrofitdemo.service.MovieApiService;
+import com.spatmar.mvvmretrofitdemo.service.RetrofitInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPopularMovies();
+    }
+
+    public void  getPopularMovies() {
+
+        MovieApiService movieApiService = RetrofitInstance.getService();
+
+        Call<MovieApiResponse> call = movieApiService
+                .getPopularMovies(getString(R.string.api_key));
+        call.enqueue(new Callback<MovieApiResponse>() {
+            @Override
+            public void onResponse(Call<MovieApiResponse> call, Response<MovieApiResponse> response) {
+
+                MovieApiResponse movieApiResponse = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<MovieApiResponse> call, Throwable t) {
+
+            }
+        });
+
+
     }
 }
